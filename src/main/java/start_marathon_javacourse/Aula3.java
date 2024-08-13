6005e369db338beaeed1b9a15d54d7ccce1fd3f5
Psiyllo <paulocesaralcabral@gmail.com>
2024-08-13 09:14:58 -0300

    aula3

---
+++ src/main/java/start_marathon_javacourse/Aula3.java
@@ -0,0 +1,13 @@
+package start_marathon_javacourse;
+
+public class Aula3 {
+    public static void main (String[] args){
+        String nome = "ja sabe né";
+        String endereco = "jucelino kubi";
+        double salario = 1412.64;
+        String dataRecebimentoSalario = "26/08/2024";
+        String relatorio = "Eu " +nome+ " morando no endereço " +endereco+" recebendo míseros "+salario+ " na data " +dataRecebimentoSalario;
+
+        System.out.println(relatorio);
+    }
+}
