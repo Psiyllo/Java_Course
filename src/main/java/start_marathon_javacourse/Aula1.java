001d7b5d24193791c4696141cdfde5a377d256fe
Psiyllo <paulocesaralcabral@gmail.com>
2024-08-13 08:54:41 -0300

    first commit

---
+++ src/main/java/start_marathon_javacourse/Aula1.java
@@ -0,0 +1,15 @@
+package start_marathon_javacourse;
+
+/**
+ * comentario <b>Top</b>
+ */
+public class Aula1 {
+    //comentario 1 linha
+    public static void main (String[] args){
+        /* comentario
+        varias
+        linhas
+         */
+        System.out.println("qualquier cosa");
+    }
+}
