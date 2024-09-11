7f46bb14066bc8e43b8deb11838b95effda54a20
Psiyllo <paulocesaralcabral@gmail.com>
2024-09-11 11:11:46 -0300

    Aula6Parte3

---
+++ src/main/java/start_marathon_javacourse/Aula6Parte3.java
@@ -0,0 +1,13 @@
+package start_marathon_javacourse;
+
+public class Aula6Parte3 {
+    public static void main(String[] args) {
+        int valorMax = 50;
+        for (int i = 0; i <= valorMax; i++){
+            if (i > 24){
+                System.out.println("i = "+ i);
+            }
+            //System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/start_marathon_javacourse/Aula6parte4.java
@@ -0,0 +1,7 @@
+package start_marathon_javacourse;
+
+public class Aula6parte4 {
+    public static void main(String[] args) {
+
+    }
+}
