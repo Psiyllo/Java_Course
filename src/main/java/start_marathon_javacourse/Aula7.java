1692b6c1253ee0b804ae515a98746d62e137eec0
Psiyllo <paulocesaralcabral@gmail.com>
2024-09-30 09:02:18 -0300

    Aula7

---
+++ src/main/java/start_marathon_javacourse/Aula7.java
@@ -0,0 +1,12 @@
+package start_marathon_javacourse;
+
+public class Aula7 {
+    public static void main(String[] args){
+        int [] idades = new int[4];
+        idades[0] = 52;
+        idades[1] = 31;
+        idades[2] = 20;
+        idades[3] = 14;
+        System.out.println(idades[2]);
+    }
+}
