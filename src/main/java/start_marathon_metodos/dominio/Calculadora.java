0fd2c280d86ffcaa1338dbaee0e856204b41ac3a
Psiyllo <paulocesaralcabral@gmail.com>
2025-01-15 11:41:37 -0300

    VarArgs

--- src/main/java/start_marathon_metodos/dominio/Calculadora.java
+++ src/main/java/start_marathon_metodos/dominio/Calculadora.java
@@ -18,4 +18,22 @@ public class Calculadora {
         }
         return num1 / num2;
     }
+
+    public void somaArray(int[] nums){
+        int soma =0;
+        for (int num : nums) {
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+
+    //se tiver mais de um atributo o int... tem que necessariamente ser o ultimo
+    public void somaVarArgs(int... nums){
+        int soma =0;
+        for (int num : nums) {
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+
 }
