a024ec465410992aea0a6620f7046a862cc5033b
Psiyllo <paulocesaralcabral@gmail.com>
2025-01-16 10:54:30 -0300

    alterando funcionario para private

--- src/main/java/start_marathon_metodos/dominio/Funcionario.java
+++ src/main/java/start_marathon_metodos/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package start_marathon_metodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void imprimirfuncionario(){
         System.out.println("Nome: " + this.nome);
@@ -20,11 +21,38 @@ public class Funcionario {
         if (salarios == null) {
             return;
         }
-        double media = 0;
             for (double salario : this.salarios) {
                 media += salario;
             }
             media /= this.salarios.length;
         System.out.println("Media Salarial: " + media);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }
--- src/main/java/start_marathon_metodos/test/FuncionarioTest.java
+++ src/main/java/start_marathon_metodos/test/FuncionarioTest.java
@@ -6,11 +6,12 @@ public class FuncionarioTest {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
 
-        funcionario.nome = "estevao";
-        funcionario.idade = 20;
-        funcionario.salarios = new double[]{1348, 922.32, 1500};
+        funcionario.setNome("estevao");
+        funcionario.setIdade(20);
+        funcionario.setSalarios(new double[]{1348, 922.32, 1500});
 
         funcionario.imprimirfuncionario();
+        System.out.println("Media: " + funcionario.getMedia());
 
     }
 }
